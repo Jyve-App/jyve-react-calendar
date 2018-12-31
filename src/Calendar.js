7556e79f3eebd6b0bfe0a601e4918b267cdee809
Tyler Hall <tyler@jyve.io>
2018-12-31 14:39:56 -0500

    feat: add spinner on calendars while gigs load

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,4 +1,8 @@
 import React, { useState, useEffect } from 'react'
+import { css } from 'emotion'
+
+import { Spin } from 'antd'
+
 import { SmallCalendar } from './SmallCalendar'
 import { LargeCalendar } from './LargeCalendar'
 
@@ -6,19 +10,40 @@ import { api } from './api'
 
 const Calendar = props => {
   const [gigs, setGigs] = useState(undefined)
+  const [loading, setLoading] = useState(true)
+
+  let spinnerClass = css`
+  .ant-spin-dot {
+    i {
+      background-color: ${props.primary || '#a89be8'}
+    }
+  }
+
+  .ant-spin-text {
+    color: ${props.primary || '#a89be8'}
+  }
+`
 
   useEffect(() => {
     let { token } = props
     api.get(`widgets`, {
       params: { access_token: token }
-    }).then(({ data }) => setGigs(data))
+    }).then(({ data }) => {
+      setGigs(data)
+      setLoading(false)
+    }).catch(() => {
+      setLoading(false)
+    })
   }, [props.token])
 
-  if (props.type === 'large') {
-    return <LargeCalendar {...props} gigs={gigs} />
-  } else {
-    return <SmallCalendar {...props} gigs={gigs} />
-  }
+  const Calendar = props.type === 'large' ? (<LargeCalendar {...props} gigs={gigs} />)
+    : (<SmallCalendar {...props} gigs={gigs} />)
+
+  return (
+    <Spin className={spinnerClass} tip='Loading Events...' spinning={loading} delay={500}>
+      {Calendar}
+    </Spin>
+  )
 }
 
 export { Calendar }
